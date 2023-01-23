4d22546912fed47d9689bcc4d914696b6aa6a34d
= <=>
2023-01-23 18:52:33 +0530

    ReactJS Demo1 , notes and diagram

---
+++ Reactjs_Demos/src_reactjs_demo1/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+
+function App() {
+  return (  // JSX
+   <h1>Hello World</h1>
+   
+  );
+}
+export default App;
---
+++ Reactjs_Demos/src_reactjs_demo1/Hello.js
@@ -0,0 +1,14 @@
+
+function  Hello(){
+
+
+
+    return(
+    <h1>Thank you friends</h1>
+    );
+
+
+}
+
+export default Hello;
+
