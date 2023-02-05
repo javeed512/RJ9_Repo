8b2ade252216ac68c6d0aa3be2d95b479ff75db5
= <=>
2023-02-05 16:28:01 +0530

    Redux Demo

---
+++ Reactjs_Demos/redux_notes/src_redux/Hello.js
@@ -0,0 +1,18 @@
+
+
+
+import React from 'react'
+
+import   {useSelector} from 'react-redux'
+
+export default function Hello() {
+
+      
+  return (
+    <div>
+
+        <h1>State value updated by App.js  </h1>
+      
+    </div>
+  )
+}
